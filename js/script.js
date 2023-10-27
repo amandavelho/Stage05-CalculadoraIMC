ed5e7857ba104751edbc755d7f409f9a1341a511
Amanda Velho <amandalimavelho@gmail.com>
2023-10-26 22:33:49 -0300

    refatorando a função de submit, clean code, finish

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 import { Modal } from "./modal.js"
 import { AlertError } from "./alert-error.js";
+import { calculateIMC, notANumber } from "./utils.js";
 
 //variables
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
+
+inputWeight.oninput = () => AlertError.close()
+inputHeight.oninput = () => AlertError.close()
 
 form.onsubmit = event => {
     event.preventDefault()
@@ -12,26 +16,22 @@ form.onsubmit = event => {
     const weight = inputWeight.value
     const height = inputHeight.value
 
-    const showAlertError = notANumber(weight) || notANumber(height)
+    const weightOrHeightIsNotANumber = notANumber(weight) || notANumber(height)
 
-    if (showAlertError) {
+    if (weightOrHeightIsNotANumber) {
         AlertError.open()
         return;
     }
 
     AlertError.close()
 
-    const result = IMC(weight, height)
+    const result = calculateIMC(weight, height)
+    displayResultMessage(result)
+}
+
+function displayResultMessage(result){
     const message = `Seu IMC é de ${result}` 
 
     Modal.message.innerText = message
     Modal.open()
-}
-
-function notANumber(value) {
-    return isNaN(value) || value == ""
-}
-
-function IMC(weight, height) {
-    return (weight / ((height / 100) ** 2)).toFixed(2)
 }
